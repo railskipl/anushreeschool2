3f7d89b4c6bd8e6df4e30150ef9bb6fe247106a2
Your Name <rails@kunalinfotech.net>
2012-04-24 14:55:07 +0530

    contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,6 +19,5 @@ class ContactsController < ApplicationController
       format.html { render :action => "new" }
       format.xml { render :xml => @contact.errors, :status => :unprocessable_entity }
     end
-   end
-  end
+    end
 end
